73a949c2530c8e2b68f23cdf00bb6f47450e918c
Wing Ming Sam <wmsam91@gmail.com>
2021-07-26 15:24:03 +0800

    update url

--- constants.go
+++ constants.go
@@ -2,16 +2,16 @@ package goisbn
 
 const (
 	GOOGLE_BOOKS_API_BASE = "https://www.googleapis.com"
-	GOOGLE_BOOKS_API_BOOK = "/books/v1/volumes"
+	GOOGLE_BOOKS_API_BOOK = "/books/v1/volumes?"
 
 	OPENLIBRARY_API_BASE = "https://openlibrary.org"
-	OPENLIBRARY_API_BOOK = "/api/books"
+	OPENLIBRARY_API_BOOK = "/api/books?"
 
 	ISBNDB_API_BASE = "https://api2.isbndb.com"
 	ISBNDB_API_BOOK = "/book"
 
 	GOODREADS_API_BASE = "https://www.goodreads.com"
-	GOODREADS_API_BOOK = "/search/index.xml"
+	GOODREADS_API_BOOK = "/search/index.xml?"
 
 	PROVIDER_GOOGLE      = "google"
 	PROVIDER_OPENLIBRARY = "openlibrary"
